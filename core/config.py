8d2e4927efc9dcd73f8ce0a5445ecc5074216e8a
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-07-27 21:50:53 +0530

    ⚙️ update config

--- core/config.py
+++ core/config.py
@@ -10,7 +10,6 @@ class GlobalConfig(BaseSettings):
     title: str = os.environ.get("TITLE")
     version: str = "1.0.0"
     description: str = os.environ.get("DESCRIPTION")
-    openapi_prefix: str = os.environ.get("OPENAPI_PREFIX")
     docs_url: str = "/docs"
     redoc_url: str = "/redoc"
     openapi_url: str = "/openapi.json"
