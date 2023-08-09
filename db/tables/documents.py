a5497af62426e8c4a4d936447566cdf839974af5
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-09 22:01:10 +0530

    update documents db

--- db/tables/documents.py
+++ db/tables/documents.py
@@ -3,19 +3,32 @@ from uuid import UUID, uuid4
 
 from fastapi import UploadFile, File
 from typing import List, Optional
-from sqlalchemy import Column, LargeBinary, String, Integer, ARRAY
+from sqlalchemy import Column, LargeBinary, String, Integer, ARRAY, text, DateTime
+from sqlalchemy.orm import Session
 
 from db.models import Base, metadata, engine
+from schemas.schemas import DocumentCreate
 
 
 class Document(Base):
     __tablename__ = "documents"
 
-    _id: UUID = Column(default=uuid4, primary_key=True, index=True, nullable=False)
-    name: Optional[str] = Column(String, unique=True)
-    data: Optional[str] = Column(LargeBinary)
-    created_at: datetime = Column(default=datetime.utcnow, nullable=False, server_default=text("NOW()"))
+    _id: UUID = Column(String, default=uuid4, primary_key=True, index=True, nullable=False)
+    name: str = Column(String, unique=True)
+    s3_url: str = Column(String, unique=True)
+    created_at: datetime = Column(DateTime, default=datetime.utcnow, nullable=False, server_default=text("NOW()"))
     size: Optional[int] = Column(Integer)
     file_type: Optional[str] = Column(String)
     tags: Optional[List[str]] = Column(ARRAY(String))
     categories: Optional[List[str]] = Column(ARRAY(String))
+
+
+def create_table(db: Session, document: DocumentCreate):
+    db_document = Document(
+        name="codeakey_logo.png",
+        s3_url="s3://docflow-trial/codeakey_logo.png"
+    )
+    db.add(db_document)
+    db.commit()
+    db.refresh(db_document)
+    return db_document
