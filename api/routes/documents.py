01128760801c805df7f21d1d83d781aaa357bb1d
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-13 15:08:54 +0530

    document repository

--- api/routes/documents.py
+++ api/routes/documents.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Body, Depends
+
+from api.dependencies.repositories import get_repository
+from db.repositories.documents import DocumentRepository
+from schemas.documents import DocumentCreate, Document
+
 
 router = APIRouter()
 
 
-@router.post()
-async def foo():
-    ...
+@router.post(
+    "/upload-document",
+    response_model=DocumentCreate,
+    status_code=status.HTTP_201_CREATED,
+    name="upload_documents"
+)
+async def upload_document(
+    document_upload: DocumentCreate = Body(...),
+    repository: DocumentRepository = Depends(get_repository(DocumentRepository)),
+)-> Document:
+    return await repository.upload(document_upload=document_upload)
