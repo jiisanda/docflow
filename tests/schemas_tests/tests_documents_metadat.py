4a1f54e3a4000c3bba59a46c82dc80c08ab2e3a6
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-08-25 14:06:51 +0530

    test schemas

---
+++ tests/schemas_tests/tests_documents_metadat.py
@@ -0,0 +1,24 @@
+import pytest
+from pydantic import ValidationError
+
+from schemas.documents_metadata import DocumentMetadataCreate
+
+
+def test_document_metadata_instance_empty():
+    with pytest.raises(expected_exception=ValidationError):
+        DocumentMetadataCreate()
+
+
+def test_document_metadata_instance_name_empty():
+    with pytest.raises(expected_exception=ValidationError):
+        DocumentMetadataCreate(s3_url="s3://docflow-test/name.test")
+
+
+def test_document_metadata_instance_s3_url_empty():
+    with pytest.raises(expected_exception=ValidationError):
+        DocumentMetadataCreate(name="name.test")
+
+
+def test_document_metadata_instance_s3_url_wrong():
+    with pytest.raises(expected_exception=ValidationError):
+        DocumentMetadataCreate(name="name.test", s3_url="s3://docflow-test")
