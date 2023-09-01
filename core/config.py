36be27e7616ad4fdf633bf46a0b88f5f3875cb6d
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-09-01 10:17:06 +0530

    update requirements and config

--- core/config.py
+++ core/config.py
@@ -21,6 +21,8 @@ class GlobalConfig(BaseSettings):
     postgres_port: int = int(os.environ.get("POSTGRES_PORT"))
     postgres_db: str = os.environ.get("POSTGRES_DB")
     db_echo_log: bool = True if os.environ.get("DEBUG") == True else False
+    aws_access_key_id: str = os.environ.get("AWS_ACCESS_KEY_ID")
+    aws_secret_key: str = os.environ.get("AWS_SECRET_ACCESS_KEY")
     s3_bucket: str = os.environ.get("S3_BUCKET")
     s3_test_bucket: str = os.environ.get("S3_TEST_BUCKET")
 
