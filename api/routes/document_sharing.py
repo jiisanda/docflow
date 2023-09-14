967edef35bccb2b0287dcb62d089d15fb9609cf1
jiisanda <harshjaiswal2307@gmail.com>
2023-09-14 13:07:55 +0530

    🔗 presigned route-repo

--- api/routes/document_sharing.py
+++ api/routes/document_sharing.py
@@ -1,14 +1,32 @@
-from fastapi import APIRouter, status
+from typing import Union
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, status, Query
+
+from api.dependencies.repositories import get_repository
+from db.repositories.documents_metadata import DocumentMetadataRepository
+from db.repositories.document_sharing import DocumentSharingRepository
+from schemas.document_sharing import SharingRequest
 
 
 router = APIRouter(tags=["Document Sharing"])
 
 
-@router.get(
-    "/",
+@router.post(
+    "/{document}",
     status_code=status.HTTP_200_OK,
     name="share_document"
 )
-async def share_document():
-    ...
+async def share_document(
+    document: Union[str, UUID],
+    share_request: SharingRequest,
+    repository: DocumentSharingRepository = Depends(DocumentSharingRepository),
+    metadata_repository: DocumentMetadataRepository = Depends(get_repository(DocumentMetadataRepository))
+):
+    doc = await metadata_repository.get(document=document)
+    presigned_url = await repository.get_presigned_url(doc=doc.__dict__)
 
+    return {
+        "personal_url": presigned_url,
+        "share_this": "tiny_url_with_visits"
+    }
--- db/repositories/document_sharing.py
+++ db/repositories/document_sharing.py
@@ -1,5 +1,34 @@
+import boto3
+from typing import Any, Dict
+
+from botocore.exceptions import NoCredentialsError
+
+from api.dependencies.repositories import _get_key
+from core.config import settings
+
 
 class DocumentSharingRepository:
 
     def __init__(self):
-        ...
+        self.s3_client = boto3.resource('s3')
+        self.client = boto3.client('s3')
+        self.s3_bucket = self.s3_client.Bucket(settings.s3_bucket)
+
+
+    async def get_presigned_url(self, doc: Dict[str, Any]):
+        try:
+            params = {
+                'Bucket': settings.s3_bucket,
+                'Key': await _get_key(s3_url=doc["s3_url"])
+            }
+            response = self.client.generate_presigned_url(
+                'get_object',
+                Params=params,
+                ExpiresIn=3600
+            )
+        except NoCredentialsError as e:
+            return {
+                "error": f"Invalid AWS Credentials: {e}"
+            }
+
+        return response
