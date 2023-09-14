59f853eabd2dcec5588551011b7187c97e8027aa
jiisanda <harshjaiswal2307@gmail.com>
2023-09-14 18:07:24 +0530

    🔗 get shareable-link

--- api/routes/document_sharing.py
+++ api/routes/document_sharing.py
@@ -20,13 +20,16 @@ router = APIRouter(tags=["Document Sharing"])
 async def share_document(
     document: Union[str, UUID],
     share_request: SharingRequest,
-    repository: DocumentSharingRepository = Depends(DocumentSharingRepository),
+    repository: DocumentSharingRepository = Depends(get_repository(DocumentSharingRepository)),
     metadata_repository: DocumentMetadataRepository = Depends(get_repository(DocumentMetadataRepository))
 ):
+
     doc = await metadata_repository.get(document=document)
+    visits = share_request.visits
     presigned_url = await repository.get_presigned_url(doc=doc.__dict__)
+    shareable_link = await repository.get_shareable_link(url=presigned_url, visits=visits)
 
     return {
         "personal_url": presigned_url,
-        "share_this": "tiny_url_with_visits"
+        "share_this": shareable_link
     }
--- db/repositories/document_sharing.py
+++ db/repositories/document_sharing.py
@@ -1,21 +1,37 @@
 import boto3
-from typing import Any, Dict
+import hashlib
+from random import randint
+from typing import Any, Dict, Union
 
 from botocore.exceptions import NoCredentialsError
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import aliased
 
 from api.dependencies.repositories import _get_key
 from core.config import settings
+from db.tables.document_sharing import DocumentSharing
+from schemas.document_sharing import DocumentSharingRead
 
 
 class DocumentSharingRepository:
 
-    def __init__(self):
-        self.s3_client = boto3.resource('s3')
+    def __init__(self, session: AsyncSession) -> None:
         self.client = boto3.client('s3')
-        self.s3_bucket = self.s3_client.Bucket(settings.s3_bucket)
+
+        self.session = session
 
 
-    async def get_presigned_url(self, doc: Dict[str, Any]):
+    async def _generate_id(self, url: str) -> str:
+        hash_object = hashlib.md5()
+        hash_object.update(url.encode('utf-8'))
+
+        n = randint(0, 25)
+
+        return hash_object.hexdigest()[n:n+6]
+
+
+    async def get_presigned_url(self, doc: Dict[str, Any]) -> Union[str, Dict[str, str]]:
         try:
             params = {
                 'Bucket': settings.s3_bucket,
@@ -32,3 +48,26 @@ class DocumentSharingRepository:
             }
 
         return response
+
+
+    async def get_shareable_link(self, url: str, visits: int):
+
+        url_id = await self._generate_id(url=url)
+        share_entry = DocumentSharing(
+            url_id=url_id,
+            url=url,
+            visits=visits
+        )
+
+        try:
+            self.session.add(share_entry)
+            await self.session.commit()
+            await self.session.refresh(share_entry)
+        except IntegrityError as e:
+            raise e
+
+        response = share_entry.__dict__
+        return {
+            "share_this_link": f"http://localhost:8000/doc/{response['url_id']}",
+            "visits": response["visits"]
+        }
