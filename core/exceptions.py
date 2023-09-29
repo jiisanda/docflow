fba9f582f3a6d8da52118f5c543cb09b0a237064
jiisanda <harshjaiswal2307@gmail.com>
2023-09-29 11:42:26 +0530

    ✨ exceptions

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,5 +1,22 @@
+from typing import Dict
+
 from fastapi.exceptions import HTTPException
 from starlette import status
+
+
+def HTTP_400(msg: str = "Bad Request...") -> HTTPException:
+    """Invalid Input"""
+    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+
+
+def HTTP_401(msg: str = "Unauthorised", headers: Dict[str, str] = None) -> HTTPException:
+    """Unauthorised Access"""
+    return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=msg, headers=headers)
+
+
+def HTTP_403(msg: str = "Forbidden") -> HTTPException:
+    """Forbidden access"""
+    return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=msg)
 
 
 def HTTP_404(msg: str = "Entity does not exists...") -> HTTPException:
@@ -10,8 +27,3 @@ def HTTP_404(msg: str = "Entity does not exists...") -> HTTPException:
 def HTTP_409(msg: str = "Entity already exists...") -> HTTPException:
     """Raised when entity already exists on database."""
     return HTTPException(status_code=status.HTTP_409_CONFLICT, detail=msg)
-
-
-def HTTP_400(msg: str = "Bad Request...") -> HTTPException:
-    """Invalid Input"""
-    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
