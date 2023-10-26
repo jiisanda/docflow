e3fa1bfdb48b6c7ea5c3e1aa510b0f303d661653
jiisanda <harshjaiswal2307@gmail.com>
2023-10-26 19:56:56 +0530

    🔔 notify: route

--- api/router.py
+++ api/router.py
@@ -5,11 +5,13 @@ from api.routes.documents.documents_metadata import router as documents_metadata
 from api.routes.documents.document import router as documents_router
 from api.routes.documents.document_organization import router as document_organization_router
 from api.routes.documents.document_sharing import router as document_sharing_router
+from api.routes.documents.notify import router as notify_router
 
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/u")
 router.include_router(documents_router, prefix="/document")
+router.include_router(notify_router, prefix="/notifications")
 router.include_router(documents_metadata_router, prefix="/document-metadata")
 router.include_router(document_organization_router, prefix="/filter")
 router.include_router(document_sharing_router)
