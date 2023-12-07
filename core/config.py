1e284a6c8f6f104e56e3a0b1f5478aa3e2efff45
jiisanda <harshjaiswal2307@gmail.com>
2023-12-08 01:11:34 +0530

    �docker-compose update

--- core/config.py
+++ core/config.py
@@ -18,7 +18,7 @@ class GlobalConfig(BaseSettings):
     debug: bool = os.environ.get("DEBUG")
     postgres_user: str = os.environ.get("POSTGRES_USER")
     postgres_password: str = os.environ.get("POSTGRES_PASSWORD")
-    postgres_server: str = os.environ.get("POSTGRES_SERVER")
+    postgres_hostname: str = os.environ.get("DATABASE_HOSTNAME")
     postgres_port: int = int(os.environ.get("POSTGRES_PORT"))
     postgres_db: str = os.environ.get("POSTGRES_DB")
     db_echo_log: bool = True if os.environ.get("DEBUG") is True else False
@@ -42,12 +42,12 @@ class GlobalConfig(BaseSettings):
     @property
     def sync_database_url(self) -> str:
         return (f"postgresql://{self.postgres_user}:{self.postgres_password}@"
-                f"{self.postgres_server}:{self.postgres_port}/{self.postgres_db}")
+                f"{self.postgres_hostname}:{self.postgres_port}/{self.postgres_db}")
 
     @property
     def async_database_url(self) -> str:
         return (f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
-                f"{self.postgres_server}:{self.postgres_port}/{self.postgres_db}")
+                f"{self.postgres_hostname}:{self.postgres_port}/{self.postgres_db}")
 
 
 settings = GlobalConfig()
